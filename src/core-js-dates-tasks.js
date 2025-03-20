6aead5be2c3802a728c323a338ffd7b6f828f850
zhuravel17 <zhuravlevakat1996@gmail.com>
2025-03-21 00:16:37 +0400

    feat: add dateToTimestamp

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -17,8 +17,8 @@
  * '01 Jan 1970 00:00:00 UTC' => 0
  * '04 Dec 1995 00:12:00 UTC' => 818035920000
  */
-function dateToTimestamp(/* date */) {
-  throw new Error('Not implemented');
+function dateToTimestamp(date) {
+  return new Date(date).getTime();
 }
 
 /**
