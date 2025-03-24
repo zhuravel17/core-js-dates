050a802dfa2b8cced65d00e7313cf48ff7386293
zhuravel17 <zhuravlevakat1996@gmail.com>
2025-03-25 01:06:24 +0400

    feat: add getQuarter

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -243,8 +243,9 @@ function getNextFridayThe13th(date) {
  * Date(2024, 5, 1) => 2
  * Date(2024, 10, 10) => 4
  */
-function getQuarter(/* date */) {
-  throw new Error('Not implemented');
+function getQuarter(date) {
+  const month = date.getMonth();
+  return Math.floor(month / 3) + 1;
 }
 
 /**
